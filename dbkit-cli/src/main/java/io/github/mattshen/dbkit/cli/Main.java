a9b13db8a56e3efc3713411ee355f6e70ef98e51
Matt Shen <lovelywib@gmail.com>
2017-09-03 10:35:56 +1000

    make project compile

--- dbkit-cli/src/main/java/io/github/mattshen/dbkit/cli/Main.java
+++ dbkit-cli/src/main/java/io/github/mattshen/dbkit/cli/Main.java
@@ -1,9 +1,21 @@
 package io.github.mattshen.dbkit.cli;
 
+import io.github.mattshen.dbkit.core.*;
+import java.util.List;
+
 public class Main {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     System.out.println("hello cli");
+
+    DbAccessor dbAccessor = DbAccessor.getInstance();
+    dbAccessor.connect();
+
+    List<String> result = dbAccessor.query("select 1", rs -> rs.getString(1));
+    System.out.println("result: " + result.get(0));
+
+    dbAccessor.close();
+
   }
 
 }
