9b3a7febf0c5348f3be1212e923c70fa9b08c7bf
Matt Shen <lovelywib@gmail.com>
2017-09-04 20:31:40 +1000

    reading from standard input

--- dbkit-cli/src/main/java/io/github/mattshen/dbkit/cli/Main.java
+++ dbkit-cli/src/main/java/io/github/mattshen/dbkit/cli/Main.java
@@ -1,28 +1,64 @@
 package io.github.mattshen.dbkit.cli;
 
-import io.github.mattshen.dbkit.cli.utils.IOUtils;
-import io.github.mattshen.dbkit.core.*;
+import io.github.mattshen.dbkit.cli.utils.Utils;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 
-import java.util.Arrays;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-  public static void main(String[] args) throws Exception {
-    System.out.println("hello cli");
+    public static void main(String[] args) throws Exception {
+        System.out.println("hello cli");
 
-    System.out.println("args=" + IOUtils.join(args, ","));
+        System.out.println("args=" + Utils.join(args, ","));
 
-    OptionParser parser = new OptionParser("h:u:p:t:");
-    OptionSet options = parser.parse(args);
+        OptionParser parser = new OptionParser("h:u:p:t:");
+        OptionSet options = parser.parse(args);
 
-    System.out.println(options.valueOf("h"));
-    System.out.println(options.valueOf("u"));
-    System.out.println(options.valueOf("p"));
-    System.out.println(options.valueOf("t"));
+        System.out.println(options.valueOf("h"));
+        System.out.println(options.valueOf("u"));
+        System.out.println(options.valueOf("p"));
+        System.out.println(options.valueOf("t"));
 
-  }
+        processStdIn();
 
+    }
+
+    static void processStdIn() {
+        BufferedReader br = null;
+
+        try {
+
+            br = new BufferedReader(new InputStreamReader(System.in));
+
+            while (true) {
+
+                System.out.print("Enter something : ");
+                String input = br.readLine();
+
+                if ("q".equals(input)) {
+                    System.out.println("Exit!");
+                    System.exit(0);
+                }
+
+                System.out.println("input : " + input);
+                System.out.println("-----------\n");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
 }
--- dbkit-cli/src/main/java/io/github/mattshen/dbkit/cli/utils/Utils.java
+++ dbkit-cli/src/main/java/io/github/mattshen/dbkit/cli/utils/Utils.java
@@ -1,16 +1,17 @@
 package io.github.mattshen.dbkit.cli.utils;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
 
 /**
  * Created by zhongweishen on 3/9/17.
  */
-public class IOUtils {
+public class Utils {
 
     public static <T> String join(T[] values, String separator) {
-        return Arrays.asList(values).stream().map(v -> v.toString()).collect(Collectors.joining(separator));
+        return Arrays.asList(values)
+                .stream().map(v -> v.toString())
+                .collect(Collectors.joining(separator));
     }
 
 }
